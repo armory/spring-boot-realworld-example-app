0253b3eda571a6d9999321b3cfa86596f10012a9
Lee Faus <lee.faus@armory.io>
2020-03-03 14:08:45 -0500

    add support for postgres

--- src/test/java/io/spring/RealworldApplicationTests.java
+++ src/test/java/io/spring/RealworldApplicationTests.java
@@ -2,6 +2,7 @@ package io.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
--- src/test/java/io/spring/application/tag/TagsQueryServiceTest.java
+++ src/test/java/io/spring/application/tag/TagsQueryServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
